f0a38a114387e90790801fdcbc5f308de2d69ed0
Charlie Thomas <cjthom03@gmail.com>
2018-06-04 16:37:14 -0700

    Complete phase 4

--- BenchBnb/db/seeds.rb
+++ BenchBnb/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Bench.delete_all
+
+Bench.create!({ description: "A bench! #1", lat: 37.768527, lng: -122.463225 })
+Bench.create!({ description: "A bench! #2", lat: 37.769199, lng: -122.440725 })
+Bench.create!({ description: "A bench! #3", lat: 37.759294, lng: -122.408588 })
+Bench.create!({ description: "A bench! #4", lat: 37.772190, lng: -122.426791 })
+Bench.create!({ description: "A bench! #5", lat: 37.764020, lng: -122.429297 })
